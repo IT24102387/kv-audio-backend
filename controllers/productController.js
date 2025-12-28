144156e8b97735f505ee13dc0e8ccd964fe51116
Kusal Chamara <kolambagekusal@gmail.com>
2025-12-29 01:07:48 +0530

    Initialized the backend repo and create authentication

---
+++ controllers/productController.js
@@ -0,0 +1,30 @@
+import Product from "../models/product.js";
+
+export function addProduct(req,res){
+   console.log(req.user)
+
+   if(req.user==null){
+      res.status(401).json({
+         message : "Please login and try again"
+      })
+      return
+
+   }
+   if(req.user.role !="admin"){
+      res.status(403).json({
+         message : "You are not authorized to perfome this action"
+      })
+      return
+
+   }
+
+   const data=req.body;
+   const newProduct=new Product(data);
+   newProduct.save()
+   .then(()=>{
+    res.json({message : "Product add successfully"});
+   })
+   .catch((error)=>{
+    res.status(500).json({error :"Prodcut adition failed"});
+   });
+}
---
+++ controllers/userController.js
@@ -0,0 +1,52 @@
+import User from "../models/user.js";
+import bcrypt from  "bcrypt"
+import jwt from "jsonwebtoken"
+
+export function registerUser(req,res){
+
+    const data=req.body;
+
+    data.password=bcrypt.hashSync(data.password,10)
+
+    const newUser=new User(data)
+
+   newUser.save().then(()=>{
+    res.json({message : "user register ed sucessfully"})
+   
+}).catch((error)=>{
+    res.status(500).json({error : "user registration failed"})
+})
+
+}
+
+export function  loginUser(req,res){
+    const data =req.body;
+
+    User.findOne({
+        email:data.email
+    }).then(
+        (user)=>{
+            if(user==null){
+                res.status(404).json({
+                    error: "user not found"
+                });
+
+            }else{
+                
+            const isPasswordCorrect=bcrypt.compareSync(data.password,user.password)
+
+            if(isPasswordCorrect){
+                const token =jwt.sign({
+                    name : user.firstName,
+                    lastname : user.lastName,
+                    email : user.email,
+                    role : user.role
+                },"kv-secret-89!")
+                res.json({message : "Login successful",token : token});
+            }else{
+                res.status(401).json({error: "Login failed"});
+            }
+         }
+        }
+    )
+}
