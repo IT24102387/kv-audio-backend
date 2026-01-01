eab98b9924b8d17627647ef609612b85e53053a6
Kusal Chamara <kolambagekusal@gmail.com>
2026-01-02 01:50:47 +0530

    implement user review backend(model,controller,routes) ❇️

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv";
+import reviewRouter from "./routes/reviewRouter.js";
 
 dotenv.config();
  
@@ -45,6 +46,7 @@ connection.once("open",()=>{
 
 app.use("/api/users",userRouter)
 app.use("/api/products",productRouter)
+app.use("/api/review",reviewRouter)
 
 
 
---
+++ models/review.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const reviewSchema=new mongoose.Schema({
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    name: {
+        type:String,
+        required: true
+    },
+    rating: {
+        type : Number,
+        required:true
+    },
+    comment : {
+        type : Number,
+        required:true
+    },
+    date: {
+        type : Date,
+        required : true,
+        default : Date.now()
+
+    },
+    isApproved : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+    profilePicture : {
+        type : String,
+        required : true,
+        default : "https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-2210.jpg?semt=ais_hybrid&w=740&q=80"
+
+    }
+
+})
+
+const Review=mongoose.model("Review",reviewSchema);
+
+export default Review;
