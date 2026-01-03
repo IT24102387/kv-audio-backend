dfc7b906f9fee5f502361edeb28859e5eb47f327
Kusal Chamara <kolambagekusal@gmail.com>
2026-01-04 01:04:51 +0530

    comple done implementing  reviews backend ⭐

--- models/review.js
+++ models/review.js
@@ -15,7 +15,7 @@ const reviewSchema=new mongoose.Schema({
         required:true
     },
     comment : {
-        type : Number,
+        type : String,
         required:true
     },
     date: {
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { addReview, getReviews } from "../controllers/reviewController.js";
+import { addReview, approveReview, deleteReview, getReviews } from "../controllers/reviewController.js";
 
 const reviewRouter=express.Router();
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReviews)
+reviewRouter.delete("/:email",deleteReview)
+
+
+// reviewRouter.get("/approved",(req,res)=>{
+//     console.log("This is  approved route ")
+// })
+
+// reviewRouter.get("/:email",(req,res)=>{
+//     console.log("This is email route ")
+// })
+
+reviewRouter.put("/approve/:email",approveReview)
+
 
 export default reviewRouter;
